52ae872d431f9076207feb36f4d192d978789b44
Ayush Saini <122162695+Ayushsaini04@users.noreply.github.com>
2023-04-19 19:44:10 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,8 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
-const movieRoute = require("./routes/movies");
-const listRoute = require("./routes/lists");
+
 
 
 
@@ -26,8 +25,7 @@ mongoose
   app.use(express.json());
   app.use("/api/auth" , authRoute);
   app.use("/api/users",userRoute);
-  app.use("/api/movies", movieRoute);
-  app.use("/api/lists", listRoute);
+
 
 
 app.listen(8800,()=> {
